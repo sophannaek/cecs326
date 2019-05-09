cee7a5cd25b5d17b40a8b1ed90018bf5bdb76776
Chelsea Marfil <chelseamarfil@yahoo.com>
2019-05-09 08:46:02 -0700

    Update main.cpp

--- semaphore/main.cpp
+++ semaphore/main.cpp
@@ -13,13 +13,15 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include "semaphore.h"
+#include <cmath>
+
 
 using namespace std;
 const int CHUNKSIZE = 512; //the size of each chunk
 const int numChunk =  3; //number of chunks in each group
 const int numGroup = 4 ; //number of groups/segments of shared memory
 int BUFFSIZE = CHUNKSIZE * numChunk;
-enum{sem1,sem2};//semaphore variables
+enum{sem1, sem2};//semaphore variables
 
 void swap(SEMAPHORE &sem, char *shmBuf,int num1, int num2);
 void parent_cleanup(SEMAPHORE &, int);
@@ -50,7 +52,8 @@ int main(int argc, const char * argv[]) {
 
 
     //create 4 groups/segments of shared memory:
-    
+    //each group holds three 512 byte chunks
+
     //allocates shared memory
     shmid1 = shmget(IPC_PRIVATE, BUFFSIZE, PERMS);
     //attach to shared memory
@@ -71,8 +74,6 @@ int main(int argc, const char * argv[]) {
     //attach to shared memory
     shmBUF4 = (char *)shmat(shmid4, 0, SHM_RND);
     
-    
-    //each group holds three 512 bytes chunk
 
     //the first group (all three chunks) will be initialized with lowercase letter
     initializeLowerChars(shmBUF1);
@@ -89,20 +90,32 @@ int main(int argc, const char * argv[]) {
 
             //Each process generate a random 32-bit random integer
             //create a random integer between 0 and 2^31 -1
-            srand(time(0));
+            srand(time(NULL));
             speed_check = rand();
             if (speed_check < 5000){
-                //randomly selects 2 groups and randomy chooses one chunk from each of
-                //these 2 groups to swap
-                int num1= rand() % 4;
-                int num2 = rand()% 4;
-                int num3 = rand()% 3;
-                int num4 = rand() %3;
+                //randomly selects 2 groups
+                int num1 = rand() % 4;
+                int num2 = rand() % 4;
+                
+                //randomly choose one chunk from each of these 2 groups to swap
+                int num3 = rand() % 3;
+                int num4 = rand() % 3;
+                
+                // get the offset for each chunk
+                if(num3==1) {
+                    int offset = 0;
+                } else if(num3==2) {
+                    int offset = 512;
+                } else if(num3==3) {
+                    int offset = 1024;
+                }
 
-                if(num1 ==num2){
+                while (num1 == num2){ // If the 2 groups are the same
                      int num2 = rand() % 4;
                 }
-                swap(sem1,shmBUF,num1,num2);
+                
+                
+                swap(sem1, shmBUF, num1, num2);
 
 
 
@@ -122,7 +135,7 @@ int main(int argc, const char * argv[]) {
 
 
 
-void swap(SEMAPHORE &sem, char *shmBuf,int num1, int num2){
+void swap(SEMAPHORE &sem, char *shmBuf, int num1, int num2){
     
 }
 void initializeLowerChars(char *shmBuf){
@@ -155,3 +168,4 @@ void parent_cleanup (SEMAPHORE &sem, int shmid) {
     shmctl(shmid, IPC_RMID, NULL);    /* cleaning up */
     sem.remove();
 } // parent_cleanup
+
